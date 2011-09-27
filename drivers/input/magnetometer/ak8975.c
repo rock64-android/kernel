8a596aa0798ddef03c32ddd84298d43aebe4f09f
lyx <lyx@rock-chips.com>
2011-09-27 00:41:46 -0700

    akm8975: add scl_rate, fix i2c err

--- drivers/input/magnetometer/ak8975.c
+++ drivers/input/magnetometer/ak8975.c
@@ -86,6 +86,8 @@ static atomic_t suspend_flag = ATOMIC_INIT(0);
 
 //static struct akm8975_platform_data *pdata;
 
+#define AKM_I2C_RATE 100*1000
+
 static int AKI2C_RxData(char *rxData, int length)
 {
 	uint8_t loop_i;
@@ -95,12 +97,14 @@ static int AKI2C_RxData(char *rxData, int length)
 			.flags = 0,
 			.len = 1,
 			.buf = rxData,
+			.scl_rate = AKM_I2C_RATE,
 		},
 		{
 			.addr = this_client->addr,
 			.flags = I2C_M_RD,
 			.len = length,
 			.buf = rxData,
+			.scl_rate = AKM_I2C_RATE,
 		},
 	};
 #if AKM8975_DEBUG_DATA
@@ -143,6 +147,7 @@ static int AKI2C_TxData(char *txData, int length)
 			.flags = 0,
 			.len = length,
 			.buf = txData,
+			.scl_rate = AKM_I2C_RATE,
 		},
 	};
 #if AKM8975_DEBUG_DATA
